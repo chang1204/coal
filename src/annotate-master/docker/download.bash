8186230f8d98c36b9436308744a5bf7ed950bd1c
chang <changsaier@icloud.com>
2023-06-29 22:09:53 +0800

    code

--- src/annotate-master/docker/download.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-version="$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev)"
-wget "http://us.download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run" -O NVIDIA-DRIVER.run
